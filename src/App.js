0a4b540c51bffa2a53f2b6139e765b142b89e388
Mateusz Choma <kontakt@coderoad.pl>
2021-12-02 19:43:35 +0100

    `onNewGameClick` and `onJoinClick` handlers implemented

--- src/App.js
+++ src/App.js
@@ -1,22 +1,56 @@
 import React from 'react'
+import { io } from 'socket.io-client'
 
 import WelcomeScreen from './components/WelcomeScreen'
 
+import { ThemeProvider, createTheme } from '@mui/material'
+
+// eslint-disable-next-line no-unused-vars
+const socket = io(process.env.REACT_APP_SOCKET_URL, { secure: true, autoConnect: false })
+
 export const App = () => {
-  const onJoinClick = React.useCallback((gameId) => {
-    console.log('onJoinClick', gameId)
-  }, [])
-  const onNewGameClick = React.useCallback(() => {
-    console.log('onNewGameClick')
+  const connectionRef = React.useRef(null)
+  const [errorMessage, setErrorMessage] = React.useState(null)
+  const [gameId, setGameId] = React.useState(null)
+
+  const onJoinClick = React.useCallback(async (gameId) => {
+    setErrorMessage(null)
+    const response = await fetch(process.env.REACT_APP_API_URL + '/join', {
+      method: 'POST',
+      headers: { 'Content-type': 'application/json' },
+      body: JSON.stringify({ gameId, connectionId: connectionRef.current })
+    })
+    const data = await response.json()
+    if (data.error) {
+      setErrorMessage(data.error)
+      return
+    }
+    setGameId(data.gameId)
   }, [])
 
+  const onNewGameClick = React.useCallback(async () => {
+    setErrorMessage(null)
+    const response = await fetch(process.env.REACT_APP_API_URL + '/new', {
+      method: 'POST',
+      headers: { 'Content-type': 'application/json' },
+      body: JSON.stringify({ connectionId: connectionRef.current })
+    })
+    const data = await response.json()
+    setGameId(data.gameId)
+  }, [])
+
+  console.log(gameId)
+
   return (
-    <div>
+    <ThemeProvider
+      theme={createTheme({ palette: { primary: { main: '#000' } } })}
+    >
       <WelcomeScreen
+        errorMessage={errorMessage}
         onJoinClick={onJoinClick}
         onNewGameClick={onNewGameClick}
       />
-    </div>
+    </ThemeProvider>
   )
 }
 
