4d66ed3416def796759f7779e743fb757c50fbb5
Mateusz Choma <kontakt@coderoad.pl>
2021-12-02 19:57:48 +0100

    `GameEndedOverlay` component added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { io } from 'socket.io-client'
 
 import WelcomeScreen from './components/WelcomeScreen'
+import GameEndedOverlay from './components/GameEndedOverlay'
 
 import { ThemeProvider, createTheme } from '@mui/material'
 
@@ -12,6 +13,8 @@ export const App = () => {
   const connectionRef = React.useRef(null)
   const [errorMessage, setErrorMessage] = React.useState(null)
   const [gameId, setGameId] = React.useState(null)
+  const [board, setBoard] = React.useState(null)
+  const [gameEnded, setGameEnded] = React.useState(false)
 
   const onJoinClick = React.useCallback(async (gameId) => {
     setErrorMessage(null)
@@ -39,7 +42,32 @@ export const App = () => {
     setGameId(data.gameId)
   }, [])
 
-  console.log(gameId)
+  const onReplayClick = React.useCallback(async () => {
+    setGameEnded(false)
+    setGameId(null)
+    setBoard(null)
+  }, [])
+
+  React.useEffect(() => {
+    socket.connect()
+
+    socket.on('connect', () => {
+      const connectionId = socket.id
+      console.log(`Connected ${process.env.REACT_APP_SOCKET_URL} with id ${connectionId}`)
+      connectionRef.current = connectionId
+    })
+
+    socket.on('connect_error', () => {
+      setTimeout(() => {
+        socket.connect()
+      }, 100)
+    })
+
+    socket.on('BOARD_CHANGED', (newBoard) => setBoard(newBoard))
+    socket.on('GAME_ENDED', () => setGameEnded(true))
+  }, [])
+
+  console.log(gameId, gameEnded, board)
 
   return (
     <ThemeProvider
@@ -50,6 +78,15 @@ export const App = () => {
         onJoinClick={onJoinClick}
         onNewGameClick={onNewGameClick}
       />
+
+      {
+        gameEnded === true ?
+          <GameEndedOverlay
+            onReplayClick={onReplayClick}
+          />
+          :
+          null
+      }
     </ThemeProvider>
   )
 }
